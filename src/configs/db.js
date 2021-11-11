628141abbc5e48d5d71b936c7a928099705c9eb4
mukundakamatham <mukundakamatham@gmail.com>
2021-11-11 12:39:45 +0530

    small updations

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-
+const config = require('config');
+const db = config.get('mongoURI');
 module.exports = () => {
-    return mongoose.connect("mongodb://localhost:27017/auth-web11")
+    return mongoose.connect(db,{"mongodb://localhost:27017/auth-web11"})
 }
